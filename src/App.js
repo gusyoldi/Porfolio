0edf3d9862b7109413b658b4f5f428283a25c628
Gus <yoldi.gustavo@gmail.com>
2025-08-07 16:54:20 -0300

    fix: hover color and animations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import "./App.css";
-import Welcome from "./components/Welcome/Welcome";
-import MyProjects from "./components/MyProjects/MyProjects";
-import Skills from "./components/Skills/Skills";
-import AboutMe from "./components/AboutMe/AboutMe";
-import ContactForm from "./components/ContactForm/ContactForm";
-import Footer from "./components/Footer/Footer";
+import './App.css'
+import Welcome from './components/Welcome/Welcome'
+import MyProjects from './components/MyProjects/MyProjects'
+import Skills from './components/Skills/Skills'
+import AboutMe from './components/AboutMe/AboutMe'
+import ContactForm from './components/ContactForm/ContactForm'
+import Footer from './components/Footer/Footer'
 
 function App() {
-	return (
-		<>
-			<Welcome />
-			<MyProjects />
-			<Skills />
-			<AboutMe />
-			<ContactForm />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Welcome />
+      <MyProjects />
+      <Skills />
+      <AboutMe />
+      <ContactForm />
+      <Footer />
+    </>
+  )
 }
 
-
-export default App;
+export default App
