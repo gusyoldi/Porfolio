62f86b33bb61264ffb617f56d02d51df760067d8
Gus <yoldi.gustavo@gmail.com>
2026-02-05 17:42:28 -0300

    feat: add new project

--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -3,6 +3,7 @@ import EdefiImg from '../../assets/images/ProjectsImg/edefi-img.png'
 import OchentaNoventaImg from '../../assets/images/ProjectsImg/ochentaNoventa.jpg'
 import PigGameImg from '../../assets/images/ProjectsImg/pig-game-img.png'
 import ReactPizzaImg from '../../assets/images/ProjectsImg/react-pizza.png'
+import TheWildOasisImg from '../../assets/images/ProjectsImg/the-wild-oasis-img.png'
 import HTML_IMG from '../../assets/images/Technologies/html.png'
 import REACT_IMG from '../../assets/images/Technologies/react.png'
 import Card from './Card/Card'
@@ -15,7 +16,14 @@ export default function MyProjects() {
       <h3>Últimos proyectos</h3>
       <div className={Styles.cardsContainer}>
         <Card
-          title="React-Pizza"
+          title="The Wild Oasis"
+          subtitle="App interna para gestionar reservas"
+          image={TheWildOasisImg}
+          toProject="https://the-wild-oasis-intern.vercel.app"
+          toGithub="https://github.com/gusyoldi/the-wild-oasis-intern"
+        />
+        <Card
+          title="React Pizza"
           subtitle="E-commerce"
           image={ReactPizzaImg}
           toProject="https://react-pizza-gus.netlify.app/"
@@ -23,20 +31,14 @@ export default function MyProjects() {
         />
         <Card
           title="Compartí un dato"
-          subtitle="App conectada a Supabase"
+          subtitle="App para compartir datos curiosos"
           image={CompartiUnDatoImg}
           toProject="https://comparti-un-dato-gustavoyoldi.netlify.app/"
           toGithub="https://github.com/gusyoldi/Today-I-learned"
         />
         <Card
-          title="Liga EDEFI"
-          subtitle="Web pública"
-          image={EdefiImg}
-          toProject="https://web.edefi.com.ar/"
-        />
-        <Card
           title="Ochenta Noventa"
-          subtitle="Landing page"
+          subtitle="Landing page de banda musical"
           image={OchentaNoventaImg}
           toProject="https://ochentanoventa.com.ar"
           toGithub="https://github.com/gusyoldi/landing-OchentaNoventa"
@@ -49,6 +51,12 @@ export default function MyProjects() {
           toProject="https://pig-game-gus.netlify.app/"
           toGithub="https://github.com/gusyoldi/pig-game"
           usedTech={usedTech}
+        />
+        <Card
+          title="Liga EDEFI"
+          subtitle="Web pública de club deportivo"
+          image={EdefiImg}
+          toProject="https://web.edefi.com.ar/"
         />
       </div>
     </section>
